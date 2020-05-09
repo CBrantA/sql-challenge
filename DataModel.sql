9616f866fab3fddf4c5e02a6ba725511413ee2b8
CBrantA <61426469+CBrantA@users.noreply.github.com>
2020-05-09 18:46:11 -0400

    Add files via upload

---
+++ DataModel.sql
@@ -0,0 +1,54 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "employees" (
+    "emp_no" -VARCHAR(30)   NOT NULL,
+    "emp_title" VARCHAR(30)   NOT NULL,
+    "birth_date" VARCHAR(30)   NOT NULL,
+    "first_name" VARCHAR(64)   NOT NULL,
+    "last_name" VARCHAR(64)   NOT NULL,
+    "sex" VARCHAR(10)   NOT NULL,
+    "hire_date" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" VARCHAR(30)   NOT NULL,
+    "dept_no" VARCHAR(30)   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(30)   NOT NULL,
+    "emp_no" VARCHAR(30)   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" VARCHAR(30)   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(30)   NOT NULL,
+    "dept_name" VARCHAR(64)   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(30)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
